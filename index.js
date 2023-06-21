716b34594e326e8b84d5917c7144e9ec84740389
悬一 <mac@XuanYi-MacBook-Air.local>
2023-06-21 13:04:50 +0800

    更新index

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ import roomsRoute from "./routes/rooms.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 
 const app = express();
